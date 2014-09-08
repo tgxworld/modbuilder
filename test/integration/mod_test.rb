580fefeef1a29ae7cba5fbd05b14cac0e738d4f7
Guo Xiang Tan <tgx_world@hotmail.com>
2014-09-08 17:36:19 +0800

    Add integrationt test for Mod#index.

--- test/integration/mod_test.rb
+++ test/integration/mod_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ModIntegrationTest < ActionDispatch::IntegrationTest
+  self.use_instantiated_fixtures = true
+
   def test_create_a_new_mod
     visit '/mods/new'
     fill_in Mod.human_attribute_name(:name), with: 'haha'
@@ -10,5 +12,12 @@ class ModIntegrationTest < ActionDispatch::IntegrationTest
 
     assert Mod.first, 'Expected a mod to be created'
     assert_equal root_path, page.current_path
+  end
+
+  def test_index
+    visit root_path
+
+    assert page.has_content?(@mod.name)
+    assert page.has_content?(@mod.code)
   end
 end
