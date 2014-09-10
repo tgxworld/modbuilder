90f8d3dd338045397df0847003cd311ab79629a5
Guo Xiang Tan <tgx_world@hotmail.com>
2014-09-10 16:30:03 +0800

    Avoid resizing image during processing.

--- app/uploaders/mod_avatar_image_uploader.rb
+++ app/uploaders/mod_avatar_image_uploader.rb
@@ -28,10 +28,6 @@ class ModAvatarImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  version :thumb do
-    process :resize_to_fit => [200, 200]
-  end
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
