19d40a09e533e721962d0cf7467749c6dbb34f15
Guo Xiang Tan <tgx_world@hotmail.com>
2014-09-10 16:46:05 +0800

    Add configuration for Carrierwave to work with S3.

--- app/uploaders/mod_avatar_image_uploader.rb
+++ app/uploaders/mod_avatar_image_uploader.rb
@@ -3,10 +3,6 @@
 class ModAvatarImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
